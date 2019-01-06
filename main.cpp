13da4d1fb4e414077fc00f2206e018c38360e9e4
Jakub <jakubowaty@wp.pl>
2019-01-06 01:32:13 +0100

    dodałem plskie znaki i poprawiłem kod obliczeń

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <math.h>
+#include <cstring>
 
 using namespace std;
+
+//ą\245ć\206ę\251ł\210ń\344ó\242ś\230ż\276ź\253Ą\244Ć\217Ę\250Ł\235Ń\343Ó\340Ś\227Ż\275Ź\215
+
+  
 int main()
  {
     int liczba, sw;
     float wynik=0;
- cout<<"podaj działanie: ";
+ cout<< "podaj dzia\210anie: ";
  cin>>sw;
     switch(sw){
      case 1:{//+
@@ -33,16 +38,16 @@ int main()
       cin>>wynik;
       while(cin>>liczba){
        if(liczba==0){
-        cout<<"nie można dzielić przez 0";
+        cout<<"nie mo\276na dzieli\206 przez 0";
         return 0;
         }
        wynik/=liczba;
        }
       break;
      }
-      deflaut: {
-       cout<<"nie wybrałeś działania";
-       break;
+      default: {
+       cout<<"nie wybra\210e\230 dzia\210ania";
+       return 0;
       }
     }
 cout<<"wynik= "<< wynik;
